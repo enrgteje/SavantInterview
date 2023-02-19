31e27a93bfc22759cf5200177a97c70910cf405a
enrgteje <enrgteje@ut.utm.edu>
2023-02-18 19:08:52 -0600

    Added a comment to send-obj service, which is how I was able to send an object from the dialog component to the table component.

--- src/app/send-obj.service.ts
+++ src/app/send-obj.service.ts
@@ -4,6 +4,8 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
+
+// Service allows to send an object from the dialog to the table component
 export class SendObjService {
   private itemData = new BehaviorSubject<any>({});
   itemData$ = this.itemData.asObservable();
